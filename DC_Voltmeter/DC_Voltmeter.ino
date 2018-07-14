3127605f74ecc0f67fae649e5c76fd1ad248e419
Meng Lin <lm8100mystery@gmail.com>
2018-07-14 13:01:03 +1000

    Voltmeter v1

--- DC_Voltmeter/DC_Voltmeter.ino
+++ DC_Voltmeter/DC_Voltmeter.ino
@@ -1,45 +1,50 @@
 /*
 DC Voltmeter 
 An Arduino DVM based on voltage divider concept
-T.K.Hareendran - Modified
+T.K.Hareendran - Modified by Meng Lin
 */
+// Source:
+// https://www.allaboutcircuits.com/projects/make-a-digital-voltmeter-using-the-arduino/
 
-int analogInput = 0; // Pin used for detection + Ground pin
+int analogInput = A0; // Pin used for detection + Ground pin
 float vout = 0.0;
 float vin = 0.0;
 float R1 = -1; // Resistance of R1 (will be changed later)
 float R2 = -1; // Resistance of R2 (will be changed later)
 int value = 0;
+
 void setup(){
    pinMode(analogInput, INPUT);
    Serial.begin(9600);
-   while (!Serial) {
-      ; // wait for serial port to connect
-   }
    Serial.println("DC VOLTMETER");
-   Serial.println("Please specify a resistance value for R1: ");
+
+   Serial.print("Please specify a valid resistance value for R1: ");
    while (R1 < 0)
    {
+      while (Serial.available()==0) {}
       R1 = Serial.parseFloat();
-      Serial.println("Please specify a valid resistance value for R1: ");
-   }
-      Serial.println("Please specify a resistance value for R2: ");
+   }  
+   Serial.println(R1);
+
+   Serial.print("Please specify a valid resistance value for R2: ");
    while (R2 < 0)
    {
-      Serial.println("Please specify a valid resistance value for R2: ");
+      while (Serial.available()==0) {}
       R2 = Serial.parseFloat();
    }
-   
+   Serial.println(R2);
+   Serial.println();
 }
+
 void loop(){
    // read the value at analog input
    value = analogRead(analogInput);
-   vout = (value * 5.0) / 1024.0; // see text
-   vin = vout / (R2/(R1+R2)); 
-   if (vin<0.09) {
-   vin=0.0;//statement to quash undesired reading !
-} 
-Serial.println("INPUT V= ");
-Serial.print(vin);
+   // the voltage entering the arduino
+   vout = (value * 5.0) / 1024.0;
+   // calculated voltage
+   vin = vout / (R1/(R1+R2)); 
+
+Serial.print("V = ");
+Serial.println(vin);
 delay(500);
 }
