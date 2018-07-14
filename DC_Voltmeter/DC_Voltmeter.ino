dc1860914d0469d9fcce8581c6378661c5fc7ad0
Meng Lin <lm8100mystery@gmail.com>
2018-07-14 11:20:36 +1000

    LED with Potentiometer finished

---
+++ DC_Voltmeter/DC_Voltmeter.ino
@@ -0,0 +1,45 @@
+/*
+DC Voltmeter 
+An Arduino DVM based on voltage divider concept
+T.K.Hareendran - Modified
+*/
+
+int analogInput = 0; // Pin used for detection + Ground pin
+float vout = 0.0;
+float vin = 0.0;
+float R1 = -1; // Resistance of R1 (will be changed later)
+float R2 = -1; // Resistance of R2 (will be changed later)
+int value = 0;
+void setup(){
+   pinMode(analogInput, INPUT);
+   Serial.begin(9600);
+   while (!Serial) {
+      ; // wait for serial port to connect
+   }
+   Serial.println("DC VOLTMETER");
+   Serial.println("Please specify a resistance value for R1: ");
+   while (R1 < 0)
+   {
+      R1 = Serial.parseFloat();
+      Serial.println("Please specify a valid resistance value for R1: ");
+   }
+      Serial.println("Please specify a resistance value for R2: ");
+   while (R2 < 0)
+   {
+      Serial.println("Please specify a valid resistance value for R2: ");
+      R2 = Serial.parseFloat();
+   }
+   
+}
+void loop(){
+   // read the value at analog input
+   value = analogRead(analogInput);
+   vout = (value * 5.0) / 1024.0; // see text
+   vin = vout / (R2/(R1+R2)); 
+   if (vin<0.09) {
+   vin=0.0;//statement to quash undesired reading !
+} 
+Serial.println("INPUT V= ");
+Serial.print(vin);
+delay(500);
+}
---
+++ beginner_s_test_1/beginner_s_test_1.ino
@@ -0,0 +1,40 @@
+// pin used for LED
+int ledPin = 13;
+// potentiometer pin
+int potPin = A0;
+float potVal = 0;
+
+// periodic flashing time
+float waitTimeOff = 5000;
+// the power number by potentiometer
+float waitFactor = 0;
+// number used to determine flash time
+float waitTimeOn;
+
+
+void setup() {
+
+  Serial.begin(9600);
+  // pin number of LED
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop() {
+
+  // read potentiometer value
+  potVal = analogRead(potPin);
+  waitFactor = potVal/1023;
+  Serial.println(waitFactor);
+  
+  if (waitFactor > 0)
+  {
+    digitalWrite(ledPin,HIGH);
+
+    // function to alter brigtness by altering flash time ratio
+    waitTimeOn = pow(waitFactor,2)*waitTimeOff;
+    
+    delayMicroseconds(waitTimeOn);
+    digitalWrite(ledPin,LOW);
+    delayMicroseconds(waitTimeOff);
+  }
+}
